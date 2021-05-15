ddc1b461ff66a11d301574b11b91530decae3d90
Maks <61799914+maaxnikolaevich@users.noreply.github.com>
2021-05-15 16:15:07 +0500

    2st commit

--- DoodleGame/Form1.Designer.cs
+++ DoodleGame/Form1.Designer.cs
@@ -29,27 +29,17 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.player = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.OliveDrab;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 629);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(485, 30);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Tag = "platform";
             // 
             // player
             // 
             this.player.BackColor = System.Drawing.Color.Yellow;
-            this.player.Location = new System.Drawing.Point(40, 582);
+            this.player.Location = new System.Drawing.Point(40, 599);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(30, 50);
             this.player.TabIndex = 1;
@@ -61,6 +51,16 @@
             this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.OliveDrab;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 632);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(485, 30);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -71,19 +71,19 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox player;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- DoodleGame/Form1.cs
+++ DoodleGame/Form1.cs
@@ -14,8 +14,8 @@ namespace DoodleGame
     {
         bool goLeft, goRight, jumping, isGameOver;
 
-        int jumpSpeed;
-        int force;
+        int jumpSpeed=10;
+        int force=8;
         int score = 0;
         int playerSpeed = 7;
 
@@ -26,6 +26,23 @@ namespace DoodleGame
         int enemyTwoSpeed = 3;
 
         private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
@@ -53,7 +70,44 @@ namespace DoodleGame
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            player.Top += jumpSpeed;
 
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (goLeft)
+            {
+                player.Left -= 5;
+            }
+
+            if (goRight)
+            {
+                player.Left += 5;
+            }
+
+            if (jumping)
+            {
+                jumpSpeed = -11;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "platform")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds) && !jumping)
+                    {
+                        force = 8;
+                        player.Top = x.Top - player.Height;
+                    }
+                }
+            }
         }
     }
 }
